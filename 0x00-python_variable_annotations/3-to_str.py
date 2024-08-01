845c245c0d48c04734760d01f841635b228c2a73
JuggernautTej <tiwatej@gmail.com>
2024-08-01 17:15:39 +0100

    populated task 3 file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This is a type-annoted function that
+takes  a float n as argument and returns
+the string representation of the float."""
+
+
+def to_str(n: float) -> str:
+    """This is a type-annoted function that
+    takes  a float n as argument and returns
+    the string representation of the float.
+    Args:
+        n: a float object.
+    Returns:
+        The floor of the float."""
+    return str(n)
