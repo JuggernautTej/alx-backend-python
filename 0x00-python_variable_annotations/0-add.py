3c2ccf39eb3401c99b9f99b8a939de70847763f5
JuggernautTej <tiwatej@gmail.com>
2024-08-01 15:10:23 +0100

    populated task 0 file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is a type-annoted function that
+takes a float a and a float b as arguments 
+and returns their sum as a float."""
+
+def add(a: float, b: float) -> float:
+    """ This type-annoted function takes
+    a float a and a float b as arguments
+    and returns their sum as a float.
+    Args:
+        a: a float object.
+        b: a float object.
+    Returns:
+        Sum of a and b as a float."""
+    return a + b
