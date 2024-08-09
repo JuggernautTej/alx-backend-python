84584611c01234540e2a1920bfcec4ba98362b09
JuggernautTej <tiwatej@gmail.com>
2024-08-09 19:45:18 +0100

    added task 4 files

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""An asynchronous routine that takes 2 int arguments
+Spawns the wait_random n times with the specified max
+delay and returns the list of all delays."""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """This is an asynchronous routine that
+    takes spawns task_wait_random n times with the specified max_delay.
+    Args:
+        n: The number of times to spawn wait_random.
+        max_delay: The maximum delay for wait_random..
+    Returns:
+        A list of all the delays in ascending order."""
+    tasks = []
+    delayed = []
+
+    for _ in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+    
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delayed.append(delay)
+    
+    return delayed
+    # tasks = [task_wait_random(max_delay) for _ in range(n)]
+    # delays = await asyncio.gather(*tasks)
+    
+    # delay_time = []
+
+    # for delay in delays:
+    #     inserted = False
+    #     for i in range(len(delay_time)):
+    #         if delay < delay_time[i]:
+    #             delay_time.insert(i, delay)
+    #             inserted = True
+    #             break
+    #         if not inserted:
+    #             delay_time.append(delay)
+    
+    # return delay_time
